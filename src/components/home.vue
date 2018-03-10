642f21a2364760d1531b107d83404c2c619e1074
tog <735417031@qq.com>
2018-03-10 21:47:53 +0800

    环境配置

---
+++ src/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+        <div class="head">aaa</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .home {
+        .head {
+            color: red;
+        }
+    }
+</style>
