4860f821ed6950955822299d5441cebbb570719e
lufangzhou <735417031@qq.com>
2018-03-13 20:31:23 +0800

    主页20%

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,8 +1,35 @@
 <template>
     <div class="home">
-        <teach-head></teach-head>
-        <div class="body-top">
-            我是身体
+        <teach-head :nickName="nickName" :headSrcLink="headSrcLink"></teach-head>
+        <div class="content">
+            <div class="content-top">
+                <div class="first-row">
+                    <el-row>
+                        <el-col :span="6">
+                            <div class="grid-content">
+                                <div class="s-row">渗透测试 <i class="el-icon-arrow-right"></i></div>
+                                <div class="s-row">渗透测试 <i class="el-icon-arrow-right"></i></div>
+                                <div class="s-row">渗透测试 <i class="el-icon-arrow-right"></i></div>
+                                <div class="s-row">渗透测试 <i class="el-icon-arrow-right"></i></div>
+                            </div></el-col>
+                        <el-col :span="18">
+                            <div class="grid-content">
+                                <div class="shuffling">
+                                    <el-carousel>
+                                        <el-carousel-item v-for="item in 4" :key="item">
+                                            <img :src="headSrcLink" alt="" height="100%" width="100%">
+                                            <div>{{ item }}</div>
+                                        </el-carousel-item>
+                                    </el-carousel>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -10,9 +37,11 @@
 <script>
     import '@/assets/scss/page/home.scss';
     export default {
-        name: 'home',
         data() {
-            return {}
+            return {
+                nickName:'tog',
+                headSrcLink:require('@/assets/images/dogHead.jpg')
+            }
         },
         components: {
         
--- src/components/templates/teachHead.vue
+++ src/components/templates/teachHead.vue
@@ -1,31 +1,60 @@
 <template>
     <div class="teach-head">
-        我是头
-        <div class="block">
-            <span>页数较少时的效果</span>
-            <el-pagination
-                    layout="prev, pager, next"
-                    :total="50">
-            </el-pagination>
+        <div class="name">
+            教学管理系统
         </div>
-        <div class="block">
-            <span >大于 7 页时的效果</span>
-            <el-pagination
-                    layout="prev, pager, next"
-                    :total="1000">
-            </el-pagination>
+        <div class="user-info">
+            <el-dropdown trigger="click">
+                  <span class="el-dropdown-link">
+                     <span class="head-img">
+                         <img :src="headSrcLink" alt="" width="100%" height="100%">
+                     </span>
+                     <span class="nick-name">{{nickName}}</span>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>个人信息</el-dropdown-item>
+                        <el-dropdown-item>注销</el-dropdown-item>
+                  </el-dropdown-menu>
+            </el-dropdown>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "teachHead"
+        name: "teachHead",
+        props:['nickName','headSrcLink']
     }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang="scss" type="text/scss">
     .teach-head{
-       color: $theme;
+       @include vertical-center;
+        justify-content: space-between;
+        padding: 0 40px;
+        height: 80px;
+        border-bottom: solid 1px $stable;
+        .name{
+            font-size: 24px;
+            font-weight: bolder;
+            letter-spacing: 6px;
+        }
+    }
+    .el-dropdown-link{
+        @include vertical-center;
+        &:hover{
+            cursor: pointer;
+        }
+        .nick-name{
+            font-size: 20px;
+        }
+        .head-img{
+            overflow: hidden;
+            display: flex;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            margin-right: 20px;
+        }
     }
 </style>
