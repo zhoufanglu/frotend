fc18d1ea0656eabecfa2143a77de84c009bb4603
Andy-yw <365456098@qq.com>
2018-04-06 11:03:18 +0800

    title - 4-6更新(接口更新)

--- src/前端数据接口.js
+++ src/前端数据接口.js
@@ -463,7 +463,7 @@ ajax({
                     user_sex:'性别',
                     user_city:[],//数组格式，省市区
                     user_autograph:'个性签名',
-                    user_score:'用户积分',
+                   // user_score:'用户积分',
                     user_headimg:'用户头像',
                     is_identity:'是否认证',
                     user_learning_course_num:"学习课程数"
